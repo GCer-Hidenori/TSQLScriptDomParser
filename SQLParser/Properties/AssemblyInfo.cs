b5c6fad472fcba8bfaa123dc6598b74de4e25fce
GCer-Hidenori <gcer.hidenori+github@gmail.com>
2020-12-08 22:45:44 +0900

    change assembly name

--- SQLParser/Properties/AssemblyInfo.cs
+++ SQLParser/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SQLParser")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2020 GCer Hidenori")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
